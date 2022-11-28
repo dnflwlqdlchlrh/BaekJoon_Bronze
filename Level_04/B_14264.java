12ff3c7c0cd7941f31cd2f903c5c244737bfb459
허윤 <huryoun5025@gmail.com>
2022-11-28 21:23:32 +0900

    정융각형과 삼각형

---
+++ Level_04/B_14264.java
@@ -0,0 +1,18 @@
+package Level_04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_14264 {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        double L = Double.parseDouble(br.readLine());
+
+        double S = (Math.sqrt(3) / 4) * Math.pow(L, 2);
+
+        System.out.println(S);
+    }
+}
