7f89a55e02e28fd5b6e7aa3389e943d111c2aeb2
허윤 <huryoun5025@gmail.com>
2022-11-29 16:52:23 +0900

    치킨 두 마리

---
+++ Level_04/L4_14489.java
@@ -0,0 +1,24 @@
+package Level_04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class L4_14489 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(br.readLine());
+
+        if(A + B < C * 2){
+            System.out.println(A + B);
+        } else {
+            System.out.println((A + B) - C * 2);
+        }
+    }
+}
