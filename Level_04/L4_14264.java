238abbfeae866435c3f6f50ce1fd80991e5de5fd
허윤 <huryoun5025@gmail.com>
2022-11-29 14:22:47 +0900

    클래스 이름 변경

--- Level_04/L4_14264.java
+++ Level_04/L4_14264.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class B_14264 {
+public class L4_14264 {
     public static void main(String[] args) throws IOException{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
---
+++ Level_04/L4_14470.java
@@ -0,0 +1,11 @@
+package Level_04;
+/**
+ * L4_14470
+ */
+public class L4_14470 {
+
+    public static void main(String[] args) {
+        
+        
+    }
+}
