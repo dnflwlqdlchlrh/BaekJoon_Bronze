c91eec84e45b49e89e436ce843676f212c258096
허윤 <huryoun5025@gmail.com>
2022-12-02 19:55:46 +0900

    폰 노이만과 파리

---
+++ Level_04/L4_14924.java
@@ -0,0 +1,23 @@
+package Level_04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class L4_14924 {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int S = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+
+        int hour = D / (S * 2);
+        int F = hour * T;
+
+        System.out.println(F);
+    }
+}
