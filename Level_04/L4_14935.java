a4ebbd1a0d8e6014ef81cacebc920f248db0ad3d
허윤 <huryoun5025@gmail.com>
2022-12-05 16:25:09 +0900

    FA

---
+++ Level_04/L4_14935.java
@@ -0,0 +1,20 @@
+package Level_04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * L4_14935
+ */
+public class L4_14935 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        br.readLine();
+        System.out.println("FA");
+
+    }
+}
