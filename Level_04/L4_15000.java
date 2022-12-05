751ede39adf98f4668340f25482d98289d5cd053
허윤 <huryoun5025@gmail.com>
2022-12-05 16:25:21 +0900

    CAPS

---
+++ Level_04/L4_15000.java
@@ -0,0 +1,18 @@
+package Level_04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class L4_15000 {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String A = br.readLine();
+
+        System.out.println(A.toUpperCase());
+        
+        
+    }
+}
