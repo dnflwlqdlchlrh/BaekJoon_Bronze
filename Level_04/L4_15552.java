06229dc7e592897cd66116ac4486c54998690f88
허윤 <huryoun5025@gmail.com>
2022-12-11 19:31:12 +0900

    빠른 A+B

---
+++ Level_04/L4_15552.java
@@ -0,0 +1,37 @@
+package Level_04;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class L4_15552 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            bw.write(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) + "\n");  
+        }
+        
+        bw.flush();
+        bw.close();
+
+        // StringBuilder Class 사용
+        // StringTokenizer st;
+        // StringBuilder sb = new StringBuilder();
+        // for(int i = 0; i < N; i++){
+        //     st = new StringTokenizer(br.readLine());
+        //     sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append("\n");
+        // }
+        // System.out.println(sb);
+        
+        
+    }
+}
