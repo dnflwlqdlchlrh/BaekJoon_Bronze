0aa75cb96b20d154aab15d57fc2417886ac1198f
허윤 <huryoun5025@gmail.com>
2022-12-15 14:05:42 +0900

    타일 채우기 4

---
+++ Level_04/L4_15700.java
@@ -0,0 +1,18 @@
+package Level_04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class L4_15700 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long N = Long.parseLong(st.nextToken());
+        long M = Long.parseLong(st.nextToken());
+
+        System.out.println((N * M) / 2);
+    }
+}
