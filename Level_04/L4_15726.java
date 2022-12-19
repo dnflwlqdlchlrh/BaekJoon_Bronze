6fcb88aadfec0d86f9dd56bc2c26f4c66749de2f
허윤 <huryoun5025@gmail.com>
2022-12-19 18:45:55 +0900

    이칙연산

---
+++ Level_04/L4_15726.java
@@ -0,0 +1,34 @@
+package Level_04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.StringTokenizer;
+
+/**
+ * L4_15726
+ */
+public class L4_15726 {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        double A = Double.parseDouble(st.nextToken());
+        double B = Double.parseDouble(st.nextToken());
+        double C = Double.parseDouble(st.nextToken());
+
+        int max = (int)Math.max((A / B) * C, (A * B) / C);
+
+        System.out.println(max);
+
+		// if(((A * B) / C) > ((A / B) * C)) {
+		// 	System.out.println((int) ((A * B) / C));
+		// }else {
+		// 	System.out.println((int) ((A / B) * C));
+		// }
+        
+    }
+    
+}
