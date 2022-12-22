769b67268f640df3a41295778772c0192056e147
허윤 <huryoun5025@gmail.com>
2022-12-22 16:45:07 +0900

    나이 계산하기

--- Level_04/L4_16199.java
+++ Level_04/L4_16199.java
@@ -25,22 +25,29 @@ public class L4_16199 {
         // 연 나이
         int age = y2 - y1;
 
-        if(y1 == y2) {
+		// 만 나이
+        if(y1 == y2) {	
+			// 만 나이와 연 나이가 0살 일 때
 			System.out.println(age);
 		}else{
-			if(m1 > m2) {
+			if(m1 > m2) {	
+				// 출생 달이 현재 달 보다 크다면 생일이 아직 지나지 않았기 때문에 연 나이 보다 한 살 어리다.
 				System.out.println(age - 1);
 			}else if(m1 == m2){
 				if(d1 <= d2) {
+					// 생일이거나 생일이 지났을 경우라면 연 나이와 만 나이가 같다
 					System.out.println(age);
 				}else {
+					// 생일이 지나기 전이라면 만 나이는 연 나이보다 한 살 어리다
 					System.out.println(age - 1);
 				}
 			}else{
 				System.out.println(age);
 			}
 		}
+		// 세는 나이(연 나이에 한 살을 더해주면 맞다.)
 		System.out.println(age + 1);
+		// 연 나이
 		System.out.println(age);
 	}
         
